e66401a407dc3fb9750c2186088bde665c1f72fd
Developer-Arefin <55868823+Developer-Arefin@users.noreply.github.com>
2020-01-07 11:25:14 +0600

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -1,64 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="icon" href="img/favicon.png" type="image/png">
-	<title>Breed Portfolio - Portfolio</title>
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="vendors/linericon/style.css">
-  <link rel="stylesheet" href="css/themify-icons.css">
-  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
-	<link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
-	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/magnific-popup.css">
-	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
-	<!-- main css -->
-	<link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-	<!--================ Start Header Area =================-->
-	<header class="header_area header_inner_page">
-		<div class="main_menu">
-			<nav class="navbar navbar-expand-lg navbar-light">
-				<div class="container">
-					<!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
-					<a class="navbar-brand logo_inner_page" href="index.html"><img src="img/logo2.png" alt=""></a>      
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-						<ul class="nav navbar-nav menu_nav">
-							<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-							<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-							<li class="nav-item active"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
-							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Pages</a>
-								<ul class="dropdown-menu">
-									<li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
-									<li class="nav-item"><a class="nav-link" href="portfolio-details.html">Portfolio Details</a></li>
-								</ul>
-							</li>
-							
-							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</div>
-	</header>
-	<!--================ End Header Area =================-->
+<?php include 'inc/header.php'; ?>
 
   <!--================ Start Banner Area =================-->
   <section class="banner_area">
@@ -103,53 +43,9 @@
 			<div class="filters-content">
 				<div class="row portfolio-grid">
 					<div class="grid-sizer col-md-3 col-lg-3"></div>
-					<div class="col-lg-6 col-md-6 all following">
+					<div class="col-lg-6 col-md-6 all popular latest following">
 						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p1.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-6 col-md-6 all latest popular upcoming">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p4.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-3 col-md-6 all latest following">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p2.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-3 col-md-6 all latest upcoming">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p3.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-6 col-md-6 all popular">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p6.jpg" alt="">
+							<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
 							<div class="overlay"></div>
 							<div class="short_info">
 								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
@@ -168,6 +64,37 @@
 							</div>
 						</div>
 					</div>
+					<div class="col-lg-6 col-md-6 all popular latest following">
+						<div class="single_portfolio">
+							<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
+							<div class="overlay"></div>
+							<div class="short_info">
+								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+								<p>Art, Illustration</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 all popular latest following">
+						<div class="single_portfolio">
+							<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
+							<div class="overlay"></div>
+							<div class="short_info">
+								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+								<p>Art, Illustration</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 all popular latest following">
+						<div class="single_portfolio">
+							<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
+							<div class="overlay"></div>
+							<div class="short_info">
+								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+								<p>Art, Illustration</p>
+							</div>
+						</div>
+					</div>
+s
 
 				</div>
 			</div>
@@ -226,49 +153,7 @@
 	<!--================ End Brands Area =================-->
 
 	
-        
-    <!--================Footer Area =================-->
-	<footer class="footer_area">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-12">
-					<div class="footer_top flex-column">
-						<div class="footer_logo">
-							<a href="#">
-								<img src="img/logo2.png" alt="">
-							</a>
-							<div class="d-lg-block d-none">
-								<nav class="navbar navbar-expand-lg navbar-light justify-content-center">
-									<div class="collapse navbar-collapse offset">
-										<ul class="nav navbar-nav menu_nav mx-auto">
-											<li class="nav-item"><a class="nav-link text-white" href="index.html">Home</a></li>
-											<li class="nav-item"><a class="nav-link text-white" href="about.html">About</a></li>
-											<li class="nav-item"><a class="nav-link text-white" href="portfolio.html">Portfolio</a></li>
-											<li class="nav-item"><a class="nav-link text-white" href="blog.html">BLog</a></li>
-											<li class="nav-item"><a class="nav-link text-white" href="services.html">Services </a></li>
-										</ul>
-									</div>
-								</nav>
-							</div>
-						</div>
-						<div class="footer_social mt-lg-0 mt-4">
-							<a href="#"><i class="fab fa-facebook-f"></i></a>
-							<a href="#"><i class="fab fa-twitter"></i></a>
-							<a href="#"><i class="fab fa-skype"></i></a>
-							<a href="#"><i class="fab fa-pinterest-p"></i></a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row footer_bottom justify-content-center">
-				<p class="col-lg-8 col-sm-12 footer-text">
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-			</div>
-		</div>
-	</footer>
-  <!--================End Footer Area =================-->
+<?php include 'inc/footer.php'; ?>
   
 
   <!-- Optional JavaScript -->
